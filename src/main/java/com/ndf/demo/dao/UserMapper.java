ff0753f792c9124b99ca31e314cccb8d10ec73c0
linchengjun <linchengjun1@xiaoniu66.com>
2018-05-02 23:33:56 +0800

    add mybatis pagehelper

--- src/main/java/com/ndf/demo/dao/UserMapper.java
+++ src/main/java/com/ndf/demo/dao/UserMapper.java
@@ -3,6 +3,8 @@ package com.ndf.demo.dao;
 import com.ndf.demo.domain.User;
 import org.apache.ibatis.annotations.*;
 
+import java.util.List;
+
 /**
  * Created by lin on 4/20/18.
  */
@@ -10,11 +12,15 @@ import org.apache.ibatis.annotations.*;
 public interface UserMapper {
 
     @Select("SELECT * FROM t_user WHERE user_name = #{userName}")
-    @Results({
+    @Results(id = "userMap", value = {
             @Result(property = "userName", column = "user_name"),
             @Result(property = "createTime", column = "create_time")
     })
     User queryByName(@Param("userName") String userName);
+
+    @Select("SELECT * FROM t_user")
+    @ResultMap("userMap")
+    List<User> findAll();
 
     @Insert("INSERT INTO t_user(user_name, password, status, create_time) VALUES(#{userName}, #{password}, #{status}, #{createTime})")
     void insert(User user);
--- src/test/java/com/ndf/demo/UserTests.java
+++ src/test/java/com/ndf/demo/UserTests.java
@@ -1,5 +1,8 @@
 package com.ndf.demo;
 
+import com.alibaba.fastjson.JSON;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.ndf.demo.dao.UserMapper;
 import com.ndf.demo.domain.User;
 import org.junit.Test;
@@ -9,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -38,6 +42,15 @@ public class UserTests {
 
         System.out.println(user);
         userMapper.insert(user);
+    }
+
+    @Test
+    public void listByPage(){
+        PageHelper.startPage(2, 2);
+        List<User> userList = userMapper.findAll();
+        PageInfo<User> pageInfo = new PageInfo<>(userList);
+        String result = JSON.toJSONString(pageInfo);
+        System.out.println(result);
     }
 
 }
